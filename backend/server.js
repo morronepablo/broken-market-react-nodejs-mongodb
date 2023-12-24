ef1b0517960628503b568efeca3bcc667b562b51
morronepablo <morronepablo@gmail.com>
2023-12-24 16:21:10 -0300

    creando proyecto mongodb y estructura de carpetas servidor

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,22 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+
+const connectToMango = () => {
+  const cs = process.env.MONGODB_CS;
+  mongoose
+    .connect(cs, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    })
+    .then((data) => {
+      console.log("successfully connected to mongodb " + data.connection.host);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+connectToMango();
 
 const port = process.env.PORT;
 
