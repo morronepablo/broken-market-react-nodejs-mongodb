a0551b799485c155304eaaacf34c7863b81c11c6
morronepablo <morronepablo@gmail.com>
2023-12-27 00:01:27 -0300

    load user and logout user funcionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,6 +7,8 @@ const {
   resetPassword,
   getUsers,
   updatePassword,
+  loadUser,
+  logoutUser,
 } = require("../controller/authController");
 
 const { isAuthenticated } = require("../middleware/auth");
@@ -17,6 +19,8 @@ router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/activate").post(activateUser);
 router.route("/forgotPassword").post(forgotPassword);
+router.route("/loadUser").post(isAuthenticated, loadUser);
+router.route("/logout").post(isAuthenticated, logoutUser);
 router.route("/resetPassword/:token").post(resetPassword);
 
 router.route("/").get(isAuthenticated, getUsers);
