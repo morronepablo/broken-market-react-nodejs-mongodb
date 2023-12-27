76bdef63cb20cb754117d232d498a313906b87a4
morronepablo <morronepablo@gmail.com>
2023-12-27 00:11:41 -0300

    correccion de fallos loadUser

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -248,7 +248,7 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 //Load user
 exports.loadUser = catchAsync(async (req, res, next) => {
   const userId = req.user.id;
-  const user = await User.findById(userId).populate("likes");
+  const user = await User.findById(userId);
 
   res.status(200).json({
     success: true,
